18b92b15cff78f86e73f07861b8c56795537eff3
Sutiksh Verma <verma.sutiksh27@gmail.com>
2021-02-15 11:17:47 +0530

    Day 12 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,26 +1,28 @@
-class Products {
-  final String id;
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "Iphone 12",
+      description: "A phone",
+      price: 999,
+      image: "",
+    )
+  ];
+}
+
+class Item {
+  final int id;
   final String name;
   final String description;
   final num price;
   final String color;
   final String image;
 
-  Products(
+  Item(
       {this.id,
       this.name,
       this.description,
       this.price,
       this.color,
       this.image});
-
-  final product = [
-    Products(
-      id: "Flutter01",
-      name: "Iphone 12",
-      description: "A phone",
-      price: 999,
-      image: "../../assets/image/login_image.png",
-    )
-  ];
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Sutiksh";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Catalog"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of Flutter by $name"),
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          }),
       drawer: MyDrawer(),
     );
   }
