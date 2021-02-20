3ca548c0ee75f24bd6202be7d636e4879e76343e
Sutiksh Verma <verma.sutiksh27@gmail.com>
2021-02-20 23:00:37 +0530

    Day 18 Completed

---
+++ lib/pages/home_detail_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomeDetailsPage extends StatelessWidget {
+  final Item catalog;
+
+  const HomeDetailsPage({Key key, this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: Vx.mOnly(right: 8),
+          children: [
+            "\$${catalog.price}".text.xl2.red800.make(),
+            ElevatedButton(
+              onPressed: () {},
+              child: "Buy".text.xl2.make(),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(MyTheme.darkBluishColor),
+                shape: MaterialStateProperty.all(StadiumBorder()),
+              ),
+            ).wh(100, 50),
+          ],
+        ).px16(),
+      ),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(
+                    tag: Key(catalog.id.toString()),
+                    child: Image.network(catalog.image))
+                .h32(context),
+            Expanded(
+              child: VxArc(
+                height: 30.0,
+                edge: VxEdge.TOP,
+                arcType: VxArcType.CONVEY,
+                child: Container(
+                  color: Colors.white,
+                  width: context.screenWidth,
+                  child: Column(
+                    children: [
+                      catalog.name.text.xl4.bold.make(),
+                      catalog.desc.text.xl
+                          .textStyle(context.captionStyle)
+                          .make(),
+                    ],
+                  ).py64(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/home_widgets/catalog_image.dart
@@ -0,0 +1,15 @@
+import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({Key key, @required this.image})
+      : assert(image != null),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image).box.p16.color(MyTheme.creamColor).make().p16();
+  }
+}
