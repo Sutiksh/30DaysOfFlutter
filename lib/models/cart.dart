af1a84b01e13a75b26b429b14e776da691004ac5
Sutiksh Verma <verma.sutiksh27@gmail.com>
2021-03-07 22:55:46 +0530

    Day 27 Completed!!

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   //catalog field
@@ -31,5 +33,15 @@ class CartModel {
   //remove item
   void remove(Item item) {
     _itemsId.remove(item.id);
+  }
+}
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+  @override
+  perform() {
+    store.cart._itemsId.add(item.id);
   }
 }
